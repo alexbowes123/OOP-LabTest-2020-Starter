e80736ff21e5aef4ed52528f40eb6552484004c5
Alex Bowes <alexbowes1@hotmail.com>
2020-04-22 11:35:36 +0100

    7th commit, added grid and colour

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -10,6 +10,7 @@ public class Gantt extends PApplet
 {	
 	int width = 800;
 	int height = 600;
+
 	public ArrayList<Task>tasks = new ArrayList<Task>();
 
 	
@@ -37,38 +38,50 @@ public class Gantt extends PApplet
 		}
 		
 	}
+	
+	public void drawGrid()
+    {
+        float border = width * 0.10f;
+
+        stroke(0, 0, 255);
+        textAlign(CENTER, CENTER);
+        for(int i = 0 ; i <= 30 ; i ++)
+        {
+            float x = map(i, 1, 30, border, width - border);
+            line(x+30, border, x+30, height);
+            fill(255);
+            text(i, x+30, 20);
+        }
+    }
 
 	public void displayTasks()
 	{
+		drawGrid();
 
 		for(int i = 0; i< tasks.size();i++)
 		{
 			Task t = tasks.get(i);
 
 			
-		//	float x = PApplet.map(i,0,tasks.size(),80,height-150);
-			float y = PApplet.map(i,0,tasks.size(),80,height -150);
+			float x = PApplet.map(i,0,tasks.size(),89,width-100);
+			float y = PApplet.map(i,0,tasks.size(),100,height-100);
 			noStroke();
+			colorMode(HSB);
+			fill(i*t.getEnd(), 255,255);
+			rect(x, y+10, t.getStart()+t.getEnd(), 30);
 			fill(255);
 			textAlign(CENTER, CENTER);
 			text(t.getTask(), 50, y);
 			
 		}
 
-
 	}
+	
 	
 	public void mousePressed()
 	{
 		println("Mouse pressed");	
 	}
-
-	/*public void mouseDragged()
-	{
-		println("Mouse dragged");
-	}*/
-
-	
 	
 	public void setup() 
 	{
