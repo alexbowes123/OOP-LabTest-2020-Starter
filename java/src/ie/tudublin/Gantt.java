c72af4f1618a8162a34ae9ec0026936a002cf5de
Alex Bowes <alexbowes1@hotmail.com>
2020-04-22 16:41:10 +0100

    removed hard coding and fixed proportionality of bars

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -11,12 +11,15 @@ public class Gantt extends PApplet
 	int width = 800;
 	int height = 600;
 
+	float border = width * 0.10f;
+
 	public ArrayList<Task>tasks = new ArrayList<Task>();
 
 	
 	public void settings()
 	{
 		size(width, height);
+		
 	}
 
 	public void loadTasks()
@@ -41,46 +44,82 @@ public class Gantt extends PApplet
 	
 	public void drawGrid()
     {
-        float border = width * 0.10f;
+       
 
         stroke(0, 0, 255);
         textAlign(CENTER, CENTER);
-        for(int i = 0 ; i <= 30 ; i ++)
+        for(int i = 1 ; i <= 30 ; i ++)
         {
-            float x = map(i, 1, 30, border, width - border);
-            line(x+30, border, x+30, height);
+            float x = map(i, 1, 30, border + 20, width - border);
+            line(x, 35, x, height - border);
             fill(255);
-            text(i, x+30, 20);
+            text(i, x, 20);
         }
     }
 
 	public void displayTasks()
 	{
 		drawGrid();
-
-		for(int i = 0; i< tasks.size();i++)
+		int i = 0;
+		float x; 
+		float x2;
+		for(Task t: tasks)
 		{
-			Task t = tasks.get(i);
 
-			
-			float x = PApplet.map(i,0,tasks.size(),89,width-100);
-			float y = PApplet.map(i,0,tasks.size(),100,height-100);
+			x = map(t.getStart(), 1, 30, border + 20, width - border);
+			x2 = map(t.getEnd(), 1, 30, border + 20, width - border);
+			float taskWidth = x2 - x;
+
+		
+			float y = map(i,0, tasks.size(), 100, height - 100);	//grand
+
+
 			noStroke();
 			colorMode(HSB);
 			fill(i*t.getEnd(), 255,255);
-			rect(x, y+10, t.getStart()+t.getEnd(), 30);
+
+			
+			rect(x,y, taskWidth, 30);
+		
 			fill(255);
 			textAlign(CENTER, CENTER);
 			text(t.getTask(), 50, y);
-			
+			i++;
 		}
-
+		
 	}
 	
 	
 	public void mousePressed()
-	{
-		println("Mouse pressed");	
+	{		
+		if(mouseX > 90 && mouseX < (width-50))
+		{
+			for(int i = 0; i< tasks.size();i++)
+			{
+				pushMatrix();
+				Task t = tasks.get(i);
+				float x = PApplet.map(i,0,tasks.size(),89,width-100);
+				float y = PApplet.map(i,0,tasks.size(),100,height-100);
+				popMatrix();
+				if(dist(mouseX, mouseY, x, y) >20 )
+				{
+					println("inside grid");
+					
+				}
+				/*
+					if mouseX and mouseY are within getStart() + 20 or getEnd() - 20
+					select the 
+
+					if getStart() - mouseX  > 20 
+					{
+						modify getStart()
+					}
+					if(getEnd() - mouseX
+				*/
+				
+			}
+		}
+		
 	}
 	
 	public void setup() 
